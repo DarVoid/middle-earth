6eb0d0ce99df252d46a410dcd859216bcd23ae80
Toby Selway <tobyselway@outlook.com>
2024-01-21 19:23:56 +0000

    Add post page
    Co-authored-by: DarVoid <DarVoid@users.noreply.github.com>

--- shire/app.vue
+++ shire/app.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="min-h-screen w-full bg-base text-blue" :class="theme">
+    <h1>Shire</h1>
+    <select v-model="theme">
+      <option value="latte">Latte</option>
+      <option value="frappe">Frappe</option>
+      <option value="macchiato">Macchiato</option>
+      <option value="mocha">Mocha</option>
+    </select>
+    <NuxtPage />
   </div>
 </template>
+
+<script setup>
+const theme = ref("frappe"); // "latte", "frappe", "macchiato", "mocha"
+</script>
