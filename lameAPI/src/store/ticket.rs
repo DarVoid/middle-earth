f2143f9ecba96b3e65e28f215a305c0a1a6c97bb
Toby Selway <tobyselway@outlook.com>
2024-02-01 23:46:54 +0000

    remove lameAPI

--- lameAPI/src/store/ticket.rs
+++
@@ -1,9 +0,0 @@
-use chrono::{DateTime, Utc};
-use rocket::serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Clone)]
-#[serde(crate = "rocket::serde")]
-pub struct Ticket {
-    pub id: String,
-    pub date: DateTime<Utc>,
-}
